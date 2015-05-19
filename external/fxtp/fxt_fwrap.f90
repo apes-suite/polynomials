0ada6919999741dc6367bbc81f03523f2b315f3a
Langhammer Kay <kay.langhammer@student.uni-siegen.de>
2015-05-19 17:14:15 +0200

    Added subroutines for the associated legendre transformation (fxtf_faltld_evl and fxtf_faltld_exp)

--- external/fxtp/fxt_fwrap.f90
+++ external/fxtp/fxt_fwrap.f90
@@ -21,6 +21,29 @@ module fxt_fwrap
       integer(kind=c_int) :: vn
       type(c_ptr), value :: w
     end subroutine fxtf_flptld_exp
+
+    subroutine fxtf_faltld_evl(v, vn, falt, m, u, un, w) bind(c)
+      use, intrinsic :: iso_c_binding
+      type(c_ptr), value :: v
+      integer(kind=c_int) :: vn
+      type(c_ptr), value :: falt
+      integer(kind=c_long) :: m
+      type(c_ptr), value :: u
+      integer(kind=c_int) :: un
+      type(c_ptr), value :: w
+    end subroutine fxtf_faltld_evl
+
+    subroutine fxtf_faltld_exp(u, un, falt, m, v, vn, w) bind(c)
+      use, intrinsic :: iso_c_binding
+      type(c_ptr), value :: u
+      integer(kind=c_int) :: un
+      type(c_ptr), value :: falt
+      integer(kind=c_long) :: m
+      type(c_ptr), value :: v
+      integer(kind=c_int) :: vn
+      type(c_ptr), value :: w
+    end subroutine fxtf_faltld_exp
+
   end interface
 
 end module fxt_fwrap
