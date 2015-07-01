33a9cad0434db875360e47e167ec209d5d326eb5
Harald Klimach <harald@klimachs.de>
2015-07-01 20:24:49 +0200

    Added a method setting to the ply_sampling to enable different strategies for the sampling.

--- source/ply_sampling_module.f90
+++ source/ply_sampling_module.f90
@@ -3,13 +3,18 @@
 !!
 !! Not to be confused with the oversample module!
 module ply_sampling_module
+  use env_module, only: labelLen
+
   use aotus_module, only: flu_state, aot_get_val, aoterr_Fatal
   use aot_table_module, only: aot_table_open, aot_table_close
 
   use tem_aux_module, only: tem_abort
   use tem_logging_module, only: logunit
+  use tem_tools_module, only: upper_to_lower
 
   implicit none
+
+  private
 
   !> This is  the data type providing the definitions for the sampling.
   type ply_sampling_type
@@ -22,7 +27,25 @@ module ply_sampling_module
     !! restricted by the global limit, due to the available space in the
     !! integers representing the treeIDs.
     integer :: max_nlevels = 0
+
+    !> Method to use for the sampling.
+    character(len=labelLen) :: method
   end type ply_sampling_type
+
+
+  public :: ply_sampling_type
+  public :: ply_sampling_load
+  public :: ply_sample_data
+
+
+  !> Private data type to describe variables with varying polynomial
+  !! representation from element to element for each variable.
+  type vdata_type
+    ! I think we need to make use of growing arrays here.
+    ! One for the space (int), nElems
+    ! One for the maxdgree (int), nElems
+    ! One for the actual data (real kind=rk), nDofs across all elements
+  end type vdata_type
 
 
 contains
@@ -62,9 +85,50 @@ contains
       write(logunit(1),*) 'ERROR: nlevels for ply_sampling needs to be an' &
         &                 // ' integer!'
       write(logunit(1),*) 'You provided nlevels but not in a form that could' &
-        &                 // ' be interpreted for a number.'
+        &                 // ' be interpreted as a number.'
       write(logunit(1),*) 'Aborting the execution, please check your config!'
       call tem_abort()
+    end if
+
+    if (me%max_nLevels > 0) then
+      !> Actually need to do sampling, read in the rest of configuration
+      !! settings.
+      call aot_get_val( L       = conf,      &
+        &               thandle = thandle,   &
+        &               key     = 'method',  &
+        &               val     = me%method, &
+        &               ErrCode = iError,    &
+        &               default = 'fixed'    )
+
+      if ( btest(iError, aoterr_Fatal) ) then
+        write(logunit(1),*) 'ERROR: method for ply_sampling needs to be an' &
+          &                 // ' string!'
+        write(logunit(1),*) 'You provided a method but not in a form that' &
+          &                 // ' could be interpreted as a string.'
+        write(logunit(1),*) 'Aborting the execution, please check your config!'
+        call tem_abort()
+      end if
+
+      me%method = adjustl(me%method)
+      me%method = upper_to_lower(me%method)
+
+      select case(trim(me%method))
+      case('fixed')
+        write(logunit(1),'(a,i0,a)') 'Sampling configured to be fixed with ', &
+          &                          me%max_nlevels, &
+          &                          ' levels.'
+      case default
+        write(logunit(1),*) 'ERROR: unknown sampling method: ', trim(me%method)
+        write(logunit(1),*) '       The sampling method needs to be one of the'
+        write(logunit(1),*) '       following:'
+        write(logunit(1),*) '       * "fixed" - will refine all elements by a'
+        write(logunit(1),*) '                   given number of levels and'
+        write(logunit(1),*) '                   provide the polynomial values'
+        write(logunit(1),*) '                   at the barycenters of those.'
+        write(logunit(1),*) ''
+        write(logunit(1),*) 'Stopping!'
+        call tem_abort()
+      end select
     end if
 
     call aot_table_close( L       = conf,   &
@@ -84,12 +148,21 @@ contains
     ! Required input: tree/subtree, to describe the original mesh.
     !                 + tracking shape to restrict generated mesh to the tracked
     !                   shape (-> Just use the tracking object?)
-    ! Arrays of data, and description of its layout FOR EACH VARIABLE:
-    !   Polynomial space and degree for each element
-    !   -> Define a datatype for this?
+    ! Variable system + initial dofs and polyspace for each variable.
     ! Output: New tree
     !         Array of data (1 dof x elements in new mesh for each variable)
     !         -> newelements x nVariables
+    ! Possibly new variable system to access the data in that array?
+
+    ! Procedure to do...
+    ! (Internally) create:
+    ! Arrays of data, and description of its layout FOR EACH VARIABLE:
+    !   Polynomial space and degree for each element
+    !   -> Define a datatype for this - see vdata_type
+
+    ! Refine the given mesh according to the configuration in the
+    ! ply_sampling_type.
+    ! And fill the final data array accordingly.
   end subroutine ply_sample_data
   !----------------------------------------------------------------------------!
   !----------------------------------------------------------------------------!
