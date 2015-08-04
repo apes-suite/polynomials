acedc11ebf33fa52cf05d42bcb518281c5deebc2
Langhammer Kay <kay.langhammer@student.uni-siegen.de>
2015-08-04 17:29:16 +0200

    some compiling bugs from utests removed
    --HG--
    branch : feature/fxt

--- utests/fxtp_n2m2n_test.f90
+++ utests/fxtp_n2m2n_test.f90
@@ -89,8 +89,13 @@ implicit none
   write(*,*) 'trafo (after n2m and m2n):', v
   write(*,*) 'Should be the same as orig.',2.*epsilon(1.0_rk)
 
-  !if (all(v(:,1) - v_orig(:) < 2.*epsilon(1.0_rk))) then
-  if (all(abs(v(:,1) - v_orig(:)) < 1e-8)) then
+  if (all(v(:,1) - v_orig < 2.*epsilon(1.0_rk))) then
+    write(*,*) 'PASSED'
+  else
+    write(*,*) 'Data does not match after conversion:'
+    write(*,*) 'FAILED'
+  end if
+
 !NA!  do power = 1,7
 !NA!    write(logUnit(10),*) '---------------------------   CHECKING CHEB->LEG L2P Q-SPACE TRAFO FOR ', 2**power
 !NA!    call check_fxt_2d(power, newRes)
@@ -120,106 +125,106 @@ implicit none
 !NA!  end if 
 !NA!  call fin_env()
 
-contains
+!contains
 
 
-  subroutine check_fxt_2d(power, res)
-    integer, intent(in) :: power
-    real(kind=rk), intent(out) :: res
-    !---------------------------------------------
-    type(ply_prj_header_type) :: header
-    type(ply_poly_project_type)   :: me
-    type(ply_prj_init_type)   :: prj_init
-    real(kind=rk),allocatable     :: nodal_data(:,:)
-    real(kind=rk), allocatable    :: modal_data(:,:)
-    real(kind=rk), allocatable    :: oversamp_modal(:,:)
-    real(kind=rk), allocatable    :: ref_modes(:)
-    type(ply_fxt_type) :: fxt
-    !-----------for init
-    integer ::basisType, maxdegree, i
-    !-----------for oversamp
-    integer :: iDegX, iDegY, dof, dofOverSamp
-
-    basisType = Q_space
-    maxdegree = power
-    header%kind = 'fxt'
-    !> todo NA: Check if nodes kind and factor are correctly used for 
-    !           fxt here or they should be different
-    header%fxt_header%nodes_header%nodes_kind = 'gauss-legendre'
-    header%fxt_header%factor = 2.0_rk
-
-    ! define my poly projection type
-    call ply_prj_init_define(me= prj_init,             &
-      &                          header = header ,         &
-      &                          maxPolyDegree= maxdegree, &
-      &                          basisType = basistype )
- 
-    ! fill the projection body according to the header
-    call ply_poly_project_fillbody(me, proj_init=prj_init,scheme_dim=2)
-  
-    allocate(ref_modes(1:me%body_2d%ndofs) ) 
-    allocate(modal_data(1:me%body_2d%ndofs,1) ) 
-    allocate(oversamp_modal(1:me%body_2d%oversamp_dofs,1) ) 
-    allocate(nodal_data(1:me%body_2d%nQuadPoints,1) ) 
-
-    do i=1, me%body_2d%ndofs 
-       ref_modes(i)=1.0/real(i, kind=rk)
-    end do
-    
-    write(*,*) "ref_modes = "
-    write(*,*) ref_modes 
-    modal_data(:,:) = 0.0_rk 
-    oversamp_modal(:,:) = 0.0_rk 
-    ! oversampling of modes 
-    do iDegX = 1, maxdegree+1
-      do iDegY = 1, maxdegree+1
-        dof = 1 + (iDegX-1) + (iDegY-1)*(maxdegree+1)
-        dofOverSamp = 1 + (iDegX-1) + (iDegY-1)*(me%oversamp_degree+1)
-        oversamp_modal(dofOverSamp,1) = ref_modes(dof)
-      end do
-    end do                  
-
-    write(*,*) "modes in overSampled space = "
-    write(*,*) oversamp_modal
- 
-    ! transform from wave to physical space
-    call ply_poly_project_m2n(me = me ,                &
-      &                       dim = 2 ,                &
-      &                       nVars = 1,               &
-      &                       nodal_data=nodal_data,   &
-      &                       modal_data=oversamp_modal)
-  
-    write(*,*) "After conversion to nodal_data = "
-    write(*,*) nodal_data
-
-    
-    oversamp_modal(:,:) = 0.0_rk
- 
-    ! ...and back again
-    ! transform from physical to wave space
-    call ply_poly_project_n2m(me = me,                  & 
-      &                       dim = 2 ,                 &
-      &                       nVars = 1,                &
-      &                       nodal_data=nodal_data,    &
-      &                       modal_data= oversamp_modal)
-    write(*,*) "After converting it back to oversampled space = "
-    write(*,*) oversamp_modal 
-
-    do iDegX = 1, maxdegree+1
-      do iDegY = 1, maxdegree+1
-        dof = 1 + (iDegX-1) + (iDegY-1)*(maxdegree+1)
-        dofOverSamp = 1 + (iDegX-1) + (iDegY-1)*(me%oversamp_degree+1)
-        modal_data(dof,:) = oversamp_modal(dofOverSamp,:)
-      end do
-    end do                  
-
-    write(*,*) "final_modal data = "
-    write(*,*) modal_data
- 
-    res= maxval( abs(ref_modes-modal_data(:,1)) ) 
-
-    write(*,*) 'power=', power
-    write(*,*) 'res=', res
-  end subroutine  check_fxt_2d
+!  subroutine check_fxt_2d(power, res)
+!    integer, intent(in) :: power
+!    real(kind=rk), intent(out) :: res
+!    !---------------------------------------------
+!    type(ply_prj_header_type) :: header
+!    type(ply_poly_project_type)   :: me
+!    type(ply_prj_init_type)   :: prj_init
+!    real(kind=rk),allocatable     :: nodal_data(:,:)
+!    real(kind=rk), allocatable    :: modal_data(:,:)
+!    real(kind=rk), allocatable    :: oversamp_modal(:,:)
+!    real(kind=rk), allocatable    :: ref_modes(:)
+!    type(ply_fxt_type) :: fxt
+!    !-----------for init
+!    integer ::basisType, maxdegree, i
+!    !-----------for oversamp
+!    integer :: iDegX, iDegY, dof, dofOverSamp
+!
+!    basisType = Q_space
+!    maxdegree = power
+!    header%kind = 'fxt'
+!    !> todo NA: Check if nodes kind and factor are correctly used for 
+!    !           fxt here or they should be different
+!    header%fxt_header%nodes_header%nodes_kind = 'gauss-legendre'
+!    header%fxt_header%factor = 2.0_rk
+!
+!    ! define my poly projection type
+!    call ply_prj_init_define(me= prj_init,             &
+!      &                          header = header ,         &
+!      &                          maxPolyDegree= maxdegree, &
+!      &                          basisType = basistype )
+! 
+!    ! fill the projection body according to the header
+!    call ply_poly_project_fillbody(me, proj_init=prj_init,scheme_dim=2)
+!  
+!    allocate(ref_modes(1:me%body_2d%ndofs) ) 
+!    allocate(modal_data(1:me%body_2d%ndofs,1) ) 
+!    allocate(oversamp_modal(1:me%body_2d%oversamp_dofs,1) ) 
+!    allocate(nodal_data(1:me%body_2d%nQuadPoints,1) ) 
+!
+!    do i=1, me%body_2d%ndofs 
+!       ref_modes(i)=1.0/real(i, kind=rk)
+!    end do
+!    
+!    write(*,*) "ref_modes = "
+!    write(*,*) ref_modes 
+!    modal_data(:,:) = 0.0_rk 
+!    oversamp_modal(:,:) = 0.0_rk 
+!    ! oversampling of modes 
+!    do iDegX = 1, maxdegree+1
+!      do iDegY = 1, maxdegree+1
+!        dof = 1 + (iDegX-1) + (iDegY-1)*(maxdegree+1)
+!        dofOverSamp = 1 + (iDegX-1) + (iDegY-1)*(me%oversamp_degree+1)
+!        oversamp_modal(dofOverSamp,1) = ref_modes(dof)
+!      end do
+!    end do                  
+!
+!    write(*,*) "modes in overSampled space = "
+!    write(*,*) oversamp_modal
+! 
+!    ! transform from wave to physical space
+!    call ply_poly_project_m2n(me = me ,                &
+!      &                       dim = 2 ,                &
+!      &                       nVars = 1,               &
+!      &                       nodal_data=nodal_data,   &
+!      &                       modal_data=oversamp_modal)
+!  
+!    write(*,*) "After conversion to nodal_data = "
+!    write(*,*) nodal_data
+!
+!    
+!    oversamp_modal(:,:) = 0.0_rk
+! 
+!    ! ...and back again
+!    ! transform from physical to wave space
+!    call ply_poly_project_n2m(me = me,                  & 
+!      &                       dim = 2 ,                 &
+!      &                       nVars = 1,                &
+!      &                       nodal_data=nodal_data,    &
+!      &                       modal_data= oversamp_modal)
+!    write(*,*) "After converting it back to oversampled space = "
+!    write(*,*) oversamp_modal 
+!
+!    do iDegX = 1, maxdegree+1
+!      do iDegY = 1, maxdegree+1
+!        dof = 1 + (iDegX-1) + (iDegY-1)*(maxdegree+1)
+!        dofOverSamp = 1 + (iDegX-1) + (iDegY-1)*(me%oversamp_degree+1)
+!        modal_data(dof,:) = oversamp_modal(dofOverSamp,:)
+!      end do
+!    end do                  
+!
+!    write(*,*) "final_modal data = "
+!    write(*,*) modal_data
+! 
+!    res= maxval( abs(ref_modes-modal_data(:,1)) ) 
+!
+!    write(*,*) 'power=', power
+!    write(*,*) 'res=', res
+!  end subroutine  check_fxt_2d
 
 end program test_fxtd_n2m2n
